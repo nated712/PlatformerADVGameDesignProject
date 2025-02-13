c8fcc373862ffc5b14159a7d5dd9f9609302eae6
NathanDecatur <NateD712@gmail.com>
2025-02-13 16:54:22 -0500

    Added PlayerMovement and camera
    yep

---
+++ AdvGDProject/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+
+    public float mouseSens = 100f;
+
+    public Transform playerBody;
+    [HideInInspector] public float xRotation = 0f;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSens;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSens;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+
+    }
+}
--- AdvGDProject/Assets/Scripts/PlayerMovement.cs
+++ AdvGDProject/Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,57 @@
+using System;
+using JetBrains.Annotations;
+using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    //reference charactercontroller from inspector    
+    public CharacterController controller;
+    //movespeed
+    [SerializeField] public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 6f;
+    //stores transform of groundCheck object in inspector
+    public Transform groundCheck;
+    //size of sphere to check if grounded
+    public float groundDistance = 0.4f;
+    //store layer that we will count as ground
+    public LayerMask groundMask;
+    bool isGrounded;
+    //for gravity calculation
+    Vector3 velocity;
+
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        //check if player is grounded
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask.value);
+
+        //reset velocity if grounded
+        if(isGrounded && velocity.y < 0){
+            velocity.y = -2f;
+        }
+
+        //set up axis for movement
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        //jump logic
+        if(Input.GetButtonDown("Jump") && isGrounded){
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        //create vector storing movement based on where player is looking
+        Vector3 move = transform.right * x + transform.forward * z;
+        //Moves character controller with created vector
+        controller.Move(move * speed * Time.deltaTime);
+
+        //gravity handling
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
+
 }
